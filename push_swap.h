a9daf6f435663292c89d36b7da99ac1c0869267e
rpisoner <144940000+rpisoner@users.noreply.github.com>
2024-01-03 18:22:07 +0100

    WE ARE STARTING

---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpisoner <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/03 15:03:59 by rpisoner          #+#    #+#             */
+/*   Updated: 2024/01/03 17:14:31 by rpisoner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <stdio.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	stack;
+
+#endif
+
+void	sa(stack a, stack b)
