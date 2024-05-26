512cd6af27a39974cfb6b5961e006ebc3efe6041
rpisoner <rpisoner@student.42madrid.com>
2024-05-26 12:54:16 +0200

    norminette

--- src/push_swap.c
+++ src/push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rpisoner <rpisoner@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: rpisoner <rpisoner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 15:04:39 by rpisoner          #+#    #+#             */
-/*   Updated: 2024/05/26 10:44:03 by rpisoner         ###   ########.fr       */
+/*   Updated: 2024/05/26 12:53:52 by rpisoner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main(int argc, char *argv[])
 {
 	t_list	*stack_a;
-	
+
 	if (!(argv[1]) || argv[1][0] == '\0')
 		error_exit();
 	if (argc == 1)
